76ccd5e5c0dde3fa21666c2993b112236e69bba8
KhamzatKot <1000000000020z@gmail.com>
2023-02-21 00:50:36 +0300

    Хочу сказать пару слов о проекте и что я сделал на этот раз. Этот блог делается для тренеровки, тут я отрабатываю свои навыки и знания. Я написал запросы на регистрацию и авторизацию в бекэнде

--- BLOG-MERN/index.js
+++ BLOG-MERN/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
+import bcrypt from "bcrypt"
 
 
 import { validationResult } from 'express-validator'
 import { registerValidation } from './validations/auth.js'
+
+import UserModel from './models/User.model.js'
 
 const app = express();
 
@@ -17,15 +20,91 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 })
 
-app.post('/login', registerValidation, (req, res) => {
-    const error = validationResult(req);
+app.post('/auth/login', async (req, res) => {
+    try {
+        const user = await UserModel.findOne({ email: req.body.email });
+
+        if (!user) {
+            return res.status(400).json({
+                message: 'Неверный логин или пароль ',
+            });
+        }
+
+
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+
+        
+        if (!isValidPass) {
+            return res.status(400).json({
+                message: 'Неверный логин или пароль ',
+            });
+        }
+
+    
+
+    const token = jwt.sign({
+        _id: user._id,
+    },'secret_key',
+    {
+        expiresIn: '30d'
+    })
+
+    const { passwordHash, ...userData} = user._doc
+
+    res.json({
+        ...userData,
+        token,
+    });
+
+
+    } catch (error) {
+        console.log(error); // тут мы выводим ошибку в кансоль для разработчика 
+        return res.status(500).json({ // ответ для пользователя 
+            messag: 'Не удалось авторизоваться '
+        })
+    }
+})
+
+app.post('/auth/register', registerValidation, async (req, res) => {
+    try {
+        const error = validationResult(req);
     if (!error.isEmpty()) {
         return res.status(400).json(error.array());
 
     }
-    res.json({
-        success: true
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const Hash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        avatarUrl: req.body.avatarUrl,
+        passwordHash: Hash,
+    });
+
+    const user = await doc.save();
+
+    const token = jwt.sign({
+        _id: user._id,
+    },'secret_key',
+    {
+        expiresIn: '30d'
     })
+
+    const { passwordHash, ...userData} = user._doc
+
+    res.json({
+        ...userData,
+        token,
+    });
+    } catch (error) {
+        console.log(error); // тут мы выводим ошибку в кансоль для разработчика 
+        return res.status(500).json({ // ответ для пользователя 
+            messag: 'Не удалось зарегестрировать пользователя'
+        })
+    }
 })
 app.listen(4000, (err) => {
     if (err) {
